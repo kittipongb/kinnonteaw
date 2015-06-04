90c7309f59985f01bcdd548fe9fdc4e5773e0669
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2015-06-04 23:42:07 +0700

    update floating-list

--- app/scripts/directives/floatingList.js
+++ app/scripts/directives/floatingList.js
@@ -1,55 +1,9 @@
 'use strict'
 
 angular.module('kinnonteawApp')
-.controller('floatingListController', ['$scope', function($scope) {
+.controller('floatingListController', ['$scope', 'contentBlockService', function ($scope, contentBlockService) {
 	//TODO: Mock POIList
-	//$scope.poiList = serviceX.fetch();
-	$scope.poiList = [
-	{
-		Id: '1', // should be MD5?
-		Name: 'ร้านกาแฟ Think Tank',
-		Location: 'สาธร กรุงเทพ',
-		Description: 'นั่งชิลๆ ได้ทั้งวัน มี Wifi มีปลั๊ก',
-		Type: 'eat',
-		ImageLink: 'images/00001.jpg',
-		Rating: '4',
-		LatLong: '',
-		ReviewsQuantity: '42',
-		ImagesQuantity: '5',
-		Address: 'address1',
-		Ranking: '',
-		Keywords: ''
-	},
-	{
-		Id: '2',
-		Name: 'วัดไชยวัฒนาราม',
-		Location: 'อยุธยา',
-		Description: 'วัดสวย และยิ่งใหญ่',
-		Type: 'place',
-		ImageLink: 'images/00002.jpg',
-		Rating: '4.5',
-		LatLong: '14.3432986, 100.5413961',
-		ReviewsQuantity: '16',
-		ImagesQuantity: '10',
-		Address: 'address2',
-		Ranking: '',
-		Keywords: ''
-	},
-	{
-		Id: '3',
-		Name: 'ที่พัก X Guesthouse',
-		Location: 'อัมพวา',
-		Description: 'ระคาประหยัด ใกล้ที่เที่ยว',
-		Type: 'sleep',
-		ImageLink: 'images/00003.jpg',
-		Rating: '3.5',
-		LatLong: '',
-		ReviewsQuantity: '2',
-		ImagesQuantity: '3',
-		Address: 'address3',
-		Ranking: '',
-		Keywords: ''
-	}];
+	$scope.poiList = contentBlockService.fetch();
 }])
 .directive('floatingList', function () {
 	return {
--- app/scripts/services/contentBlocksService.js
+++ app/scripts/services/contentBlocksService.js
@@ -1,34 +1,61 @@
 'use strict';
 
-var contentBlocksService = angular.module('contentBlocksService', []);
-
-contentBlocksService.factory('ContentBlock', [
+angular.module('kinnonteawApp').factory('contentBlockService', [
   function() {
   	var content = {};
   	content.blocks = [
-  		{
-  			Content: 'Block1',
-  			Type: 'Place',
-  			POI: '00001'
-  		},
-  		{
-  			Content: 'Block2',
-  			Type: 'Eat',
-  			POI: '00002'
-  		},
-  		{
-  			Content: 'Block3',
-  			Type: 'Place',
-  			POI: '00003'
-  		}
-  	];
+    {
+      Id: '1', // should be MD5?
+      Name: 'ร้านกาแฟ Think Tank',
+      Location: 'สาธร กรุงเทพ',
+      Description: 'นั่งชิลๆ ได้ทั้งวัน มี Wifi มีปลั๊ก',
+      Type: 'eat',
+      ImageLink: 'images/00001.jpg',
+      Rating: '4',
+      LatLong: '',
+      ReviewsQuantity: '42',
+      ImagesQuantity: '5',
+      Address: 'address1',
+      Ranking: '',
+      Keywords: ''
+    },
+    {
+      Id: '2',
+      Name: 'วัดไชยวัฒนาราม',
+      Location: 'อยุธยา',
+      Description: 'วัดสวย และยิ่งใหญ่',
+      Type: 'place',
+      ImageLink: 'images/00002.jpg',
+      Rating: '4.5',
+      LatLong: '14.3432986, 100.5413961',
+      ReviewsQuantity: '16',
+      ImagesQuantity: '10',
+      Address: 'address2',
+      Ranking: '',
+      Keywords: ''
+    },
+    {
+      Id: '3',
+      Name: 'ที่พัก X Guesthouse',
+      Location: 'อัมพวา',
+      Description: 'ระคาประหยัด ใกล้ที่เที่ยว',
+      Type: 'sleep',
+      ImageLink: 'images/00003.jpg',
+      Rating: '3.5',
+      LatLong: '',
+      ReviewsQuantity: '2',
+      ImagesQuantity: '3',
+      Address: 'address3',
+      Ranking: '',
+      Keywords: ''
+    }];
 
   	var query = function () {
   		return content;
   	};
 
     return {
-    	query: query
+    	fetch: query
     };
     
   }]);

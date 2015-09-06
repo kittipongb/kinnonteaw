cb90e0705ee23a445e985b3b561d1d54948e1764
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2015-09-06 17:23:39 +0700

    add poiview page

---
+++ app/scripts/controllers/poiview.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kinnonteawApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kinnonteawApp
+ */
+angular.module('kinnonteawApp')
+  .controller('PoiViewCtrl', ['$scope', function ($scope) {
+    $scope.greeting = 'Hello from PoiViewCtrl';
+  }]);

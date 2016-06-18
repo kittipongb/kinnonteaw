0d0f36151d07efad665d4083c3d835e6dd8c6534
panyaxbo <panya.javamania@gmail.com>
2016-06-18 12:50:11 +0700

    adjust layout journey card

--- service/mongodb/server.js
+++ service/mongodb/server.js
@@ -59,10 +59,19 @@ mongodb.MongoClient.connect(config.connection_url + config.collection_name, func
 */
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use( express.static( __dirname + '/') );
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
 
 app.get('/', function(req, res, next) {
